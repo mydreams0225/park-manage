9f812b7f03af7b35f35db050e6be40f518338802
zlz <1224893829@qq.com>
2018-07-30 20:47:26 +0800

    27commit

--- src/page/merchantManage/businessListings.vue
+++ src/page/merchantManage/businessListings.vue
@@ -1,11 +1,13 @@
 <template>
     <section>
         <div class="parent">
+            <!-- 操作区 -->
             <div class="margin-tops">
-                <el-button type="primary"><i class="el-icon-plus"></i> 新增</el-button>
-                <el-button type="denger"><i class="el-icon-delete"></i> 删除</el-button>
+                <el-button type="primary" size="medium"><i class="el-icon-plus"></i> 新增</el-button>
+                <el-button type="denger" size="medium"><i class="el-icon-delete"></i> 删除</el-button>
 
             </div>
+            <!-- 查询区 -->
             <div class="margin-tops querys">
                 <span>所属停车场</span>
                 <el-select v-model="filters.park" >
@@ -17,7 +19,32 @@
                         >
                     </el-option>
                 </el-select>
-                
+                <el-input placeholder="商户名称"  v-model="filters.bussinessName">
+                     <template slot="prepend">商户名称</template>   
+                </el-input>
+                <el-input placeholder="门牌号"  v-model="filters.doorNumber">
+                     <template slot="prepend">门牌号</template>   
+                </el-input>
+                <el-button type="primary" size="medium"><i class="el-icon-search"></i>查询</el-button>
+                <el-button size="medium"><i class="el-icon-delete"></i> 清除</el-button>
+            </div>
+            <!-- 展示区 -->
+            <div class="margin-tops">
+               <el-table
+               :data="businessList"
+               tool-tip-effect="dark"
+               style="100%"
+               @selection-change="handleselectchange">
+                   <el-table-column
+                        type="selection"
+                        width="55">
+                    </el-table-column>
+                    <el-table-column
+                        prop="name"
+                        label="姓名"
+                        width="120">
+                    </el-table-column>
+               </el-table>
             </div>
         </div>
     </section>
